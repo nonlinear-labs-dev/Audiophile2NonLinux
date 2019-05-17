5a87e9e676cc01cae9decb46a5f064d8eccd1f86
Henry Hoegelow <henry@hoegelow.com>
2019-05-17 08:52:44 +0200

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -4,6 +4,7 @@ echo "Starting transormation of AP Linux into Nonlinux..."
 
 if [ $(fdisk -l /dev/sda | wc -l) != "4" ]
  echo "/dev/sda is already partitioned - exit" && exit 1
+fi
  
 echo "Partitioning /dev/sda"
 curl -L "https://github.com/nonlinear-labs-dev/Audiophile2NonLinux/raw/master/sda.sfdisk" | sfdisk /dev/sda
