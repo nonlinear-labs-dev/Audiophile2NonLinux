52f4d1fa62bd5143a63d27a94c69c15dc75e919c
Henry Hoegelow <henry@hoegelow.com>
2019-05-17 08:34:35 +0200

    Create runme.sh

---
+++ runme.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ $(fdisk -l /dev/sda | wc -l) != "4" ]
+ echo "/dev/sda is already partitioned - exit" && exit 1
+
+
+
+
+mkfs.ext4 /dev/sdXX
+# mount /dev/sdXX /mnt
+# time cp -ax / /mnt
+# arch-chroot /mnt /bin/bash
+# cd /etc/apl-files
+# ./runme.sh
+# grub-install --target=i386-pc /dev/sdX 
+# grub-mkconfig -o /boot/grub/grub.cfg
+# passwd root
+# ln -s /usr/share/zoneinfo/Europe/Dublin /etc/localtime
+# hwclock --systohc --utc
+# ./autologin.sh
+# exit
+# genfstab -U /mnt >> /mnt/etc/fstab
+# reboot
