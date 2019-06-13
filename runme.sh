2afa39654b32e915e037f68692620ef2a80b44fb
Henry Hoegelow <henry@hoegelow.com>
2019-06-13 12:35:16 +0200

    wip

--- runme.sh
+++ runme.sh
@@ -60,13 +60,13 @@ arch-chroot /mnt /bin/bash -c "mount ${SSD}3 /mnt/update"
 arch-chroot /mnt /bin/bash -c "rm -rf /mnt/update/pkg"
 arch-chroot /mnt /bin/bash -c "touch /mnt/update/NonLinux.pkg.tar.gz"
 arch-chroot /mnt /bin/bash -c "wget --tries=1 --timeout=5 'http://192.168.2.180:8000/NonLinux.pkg.tar.gz' -O /mnt/update/NonLinux.pkg.tar.gz"
-sleep 20
-arch-chroot /mnt /bin/bash -c "if [ ! -s /mnt/update/NonLinux.pkg.tar.gz ]; then wget --tries=1 --timeout=5 'http://185.28.186.202:8000/NonLinux.pkg.tar.gz; fi' -O /mnt/update/NonLinux.pkg.tar.gz"
-sleep 20
-arch-chroot /mnt /bin/bash -c "if [ ! -s /mnt/update/NonLinux.pkg.tar.gz ]; then wget --tries=1 --timeout=5 'https://github.com/nonlinear-labs-dev/Audiophile2NonLinux/releases/download/1.0/NonLinux.pkg.tar.gz; fi' -O /mnt/update/NonLinux.pkg.tar.gz"
-sleep 20
+sleep 5
+arch-chroot /mnt /bin/bash -c "if [ ! -s /mnt/update/NonLinux.pkg.tar.gz ]; then wget --tries=1 --timeout=5 'http://185.28.186.202:8000/NonLinux.pkg.tar.gz' -O /mnt/update/NonLinux.pkg.tar.gz; fi"
+sleep 5
+arch-chroot /mnt /bin/bash -c "if [ ! -s /mnt/update/NonLinux.pkg.tar.gz ]; then wget --tries=1 --timeout=5 'https://github.com/nonlinear-labs-dev/Audiophile2NonLinux/releases/download/1.0/NonLinux.pkg.tar.gz' -O /mnt/update/NonLinux.pkg.tar.gz; fi"
+sleep 5
 arch-chroot /mnt /bin/bash -c "tar -C /mnt/update -xzf /mnt/update/NonLinux.pkg.tar.gz"
-sleep 20
+sleep 5
 arch-chroot /mnt /bin/bash -c "echo 'Server = file:///mnt/update/pkg/' > /etc/pacman.d/mirrorlist"
 
 echo "Remove unnecessary packages:"
