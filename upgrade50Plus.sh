cc42cb03b3e1606ec35394c8b8e8bdce60626743
Henry <henry@hoegelow.com>
2019-09-12 15:36:55 +0200

    finalized strings

--- upgrade50Plus.sh
+++ upgrade50Plus.sh
@@ -2,43 +2,19 @@
 
 IP=$1
 
-clear_displays() {
-    if [ -e /nonlinear/text2soled/text2soled ]; then
-        /nonlinear/text2soled/text2soled clear
-    fi
+t2s() {
+    /nonlinear/text2soled/t2o multitext "-- Do not switch off! --@s2c" "$1" "$2" "$3" "$4" "$5" "$6"
 }
 
-boled() {
-    if [ -e /nonlinear/text2soled/text2soled ]; then
-        /nonlinear/text2soled/text2soled "$1" 0 36
-    else
-        echo "BOLED MESSAGE: $1"
-    fi
-}
+pretty() {
+    echo "pretty arguments: $*"
+    HEADLINE="$1"
+    BOLED_LINE_1="$2"
+    BOLED_LINE_2="$3"
+    SOLED_LINE_1="$4"
+    SOLED_LINE_2="$5"
 
-soled() {
-    if [ -e /nonlinear/text2soled/text2soled ]; then
-        /nonlinear/text2soled/text2soled "$1" 0 84
-    else
-        echo "SOLED MESSAGE: $1"
-    fi
-}   
-
-cout() {
-    SOLED="$1"
-    BOLED="$2"
-    
-    if [ -z "$BOLED" ]; then
-        BOLED="$SOLED";
-    fi
-
-    if [ -z "$BOLED" ]; then
-        return 1
-    fi
-
-    clear_displays
-    boled "$BOLED"
-    soled "$SOLED"
+    t2s "${HEADLINE}@b1c" "${BOLED_LINE_1}@b3c" "${BOLED_LINE_2}@b4c" "${SOLED_LINE_1}@s0c" "${SOLED_LINE_2}@s1c"
 }
 
 executeAsRoot() {
@@ -47,7 +23,7 @@ executeAsRoot() {
 }
 
 quit() {
-    cout "$1"
+    pretty "$1" "$2" "$3" "$4" "$5" "$6"
     exit 1
 }
 
@@ -63,7 +39,7 @@ print_scp_progress() {
     while [ ! "$TARGET_SIZE_BYTES" = "$SOURCE_SIZE_BYTES" ]; do
         TARGET_SIZE=$(sshpass -p 'sscl' ssh sscl@$IP "ls -lah $3 | awk {'print \$5'}")
         TARGET_SIZE_BYTES=$(sshpass -p 'sscl' ssh sscl@$IP "ls -la $3 | awk {'print \$5'}")
-        cout "$1" "copying $TARGET_SIZE of $SOURCE_SIZE"
+        pretty "Copying..." "partition $1 content" "$TARGET_SIZE of $SOURCE_SIZE" "Copying partition $1" "$TARGET_SIZE / $SOURCE_SIZE"
         sleep 1
     done
 }
@@ -74,35 +50,36 @@ print_dd_progress() {
     touch $FILE
     while [ -e "$FILE" ]; do
         OUT=$(cat $FILE | tr '\r' '\n' | tail -n1 | grep -o "[0-9]* bytes")
-        cout "$1" "dumping $OUT"
+        pretty "Dumping..." "partition $1 content" "$OUT Bytes." "Dumping partition $1" "$OUT Bytes."
         sleep 1 
     done    
 } 
 
 
 check_preconditions() {
-    cout "Checking preconditions..."
-    [ -z "$IP" ] && quit "usage: $0 <IP-of-ePC>"
-    ping -c1 $IP 1>&2 > /dev/null || quit "ePC is not reachable at $IP, update failed."
-    executeAsRoot "sfdisk -d /dev/sda | grep sda5 | grep 42049536" || quit "ePC partition 5 is not expected position, update failed."
-    executeAsRoot "sfdisk -d /dev/sda | grep sda5 | grep 20482422" || quit "ePC partition 5 is not of expected size, update failed."
-    cout "Checking preconditions done."
+    pretty "Checking preconditions..." "" "" "Checking" "preconditions..."
+    [ -z "$IP" ] && quit "Usage:" "$0 <IP-of-ePC>" "" "wrong usage" ""
+    ping -c1 $IP 1>&2 > /dev/null || quit "No connection!" "ePC is not reachable at" "$IP, update failed." "Update failed." "ePC not reachable."
+    executeAsRoot "sfdisk -d /dev/sda | grep sda5 | grep 42049536" || quit "Unexpected partition" "ePC partition 5 is not at" "expected position, update failed." "Update failed." "partition error"
+    executeAsRoot "sfdisk -d /dev/sda | grep sda5 | grep 20482422" || quit "Unexpected partition" "ePC partition 5 is not of" "expected size, update failed." "Update failed." "partition error"
+    pretty "Checking preconditions" "done." "" "Checking preconditions" "done."
 }
 
 tear_down_playground() {
+    pretty "Stopping C15 processes..." "" "" "Stopping C15" "processes..."
     systemctl stop playground
     systemctl stop bbbb
-    cout "Nonlinear Labs processes stopped."
+    pretty "Stopping C15 processes" "done." "" "Stopping C15" "processes done."
 }
 
 unmount_doomed() {
-    cout "Unmounting partitions..."
-    executeAsRoot "umount /boot/efi" || quit "unmounting efi partition failed, update failed."
-    cout "Unmounting partitions done."
+    pretty "Unmounting partitions..." "" "" "Unmounting partitions..." ""
+    executeAsRoot "umount /boot/efi" || quit "Unmounting failed." "Unmounting /boot/efi failed," "update failed." "Update failed:" "umount failed."
+    pretty "Unmounting partitions done." "" "" "Unmounting partitions" "done."
 }
 
 create_partitions() {
-    cout "Creating partitions..."
+    pretty "Creating partitions..." "" "" "Creating partitions..." ""
     PART="label: gpt
           label-id: 7D22A5F5-C3A7-4C35-B879-B58C9B422919
           device: /dev/sda
@@ -115,78 +92,86 @@ create_partitions() {
           /dev/sda4 : start=    34605056, size=     7444480, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, uuid=22c47cae-cf10-11e9-b217-6b290f556266
           /dev/sda5 : start=    42049536, size=    20482422, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, uuid=45c5a8ae-cf10-11e9-aefa-5f647edf4354"
     
-    executeAsRoot "echo \"$PART\" | sfdisk --no-reread /dev/sda" || quit "Could not repartition the ePC SSD, update failed."
-    executeAsRoot "partprobe" || quit "Could not re-read the SSD partition table. Update failed, device probably bricked."
-    executeAsRoot "mkfs.fat /dev/sda1" || quit "Could not create partition 1. Update failed, device probably bricked."
-    executeAsRoot "mkfs.ext4 /dev/sda2" || quit "Could not create partition 2. Update failed, device probably bricked."
-    executeAsRoot "mkfs.ext4 /dev/sda3" || quit "Could not create partition 3. Update failed, device probably bricked."
-    executeAsRoot "mkfs.ext4 /dev/sda4" || quit "Could not create partition 4. Update failed, device probably bricked."
-    cout "Creating partitions done."
+    executeAsRoot "echo \"$PART\" | sfdisk --no-reread /dev/sda" || quit "Partitioning failed." "Could not repartition the" "ePC SSD, update failed." "Update failed:" "Partitioning failed."
+    executeAsRoot "partprobe" || quit "Partitioning" "Could not re-read the SSD" "partition table. Update failed" "Update failed:" "re-read part. table failed"
+    executeAsRoot "mkfs.fat /dev/sda1" || quit "Partitioning" "Could not make filesystem." "Update failed." "Update failed:" "mkfs part 1 failed"
+    executeAsRoot "mkfs.ext4 /dev/sda2" || quit "Partitioning" "Could not make filesystem." "Update failed." "Update failed:" "mkfs part 2 failed"
+    executeAsRoot "mkfs.ext4 /dev/sda3" || quit "Partitioning" "Could not make filesystem." "Update failed." "Update failed:" "mkfs part 3 failed"
+    executeAsRoot "mkfs.ext4 /dev/sda4" || quit "Partitioning" "Could not make filesystem." "Update failed." "Update failed:" "mkfs part 4 failed"
+    pretty "Creating partitions done." "" "" "Creating partitions" "done."
 }
 
 copy_partition_content() {
-    cout "Copying partitions content..." "Mounting partition 3...."
-    executeAsRoot "mount /dev/sda3 /mnt" || quit "Could not mount partition 3, update failed, device probably bricked."
-    cout "Copying partitions content..." "Chmod partition 3...."
-    executeAsRoot "chmod 777 /mnt" || quit "Could not chmod partition 3, update failed, device probably bricked."
-    cout "Copying partition 1 content..." "Copying partition 1 content to temporary storage...."
-    print_scp_progress "Copying partition 1 content..." ./p1.raw.gz /mnt/p1.raw.gz &
-    sshpass -p 'sscl' scp ./p1.raw.gz sscl@$IP:/mnt || quit "Could not copy partition 1 content onto device. Update failed, device probably bricked."
-    cout "Copying partition 2 content..." "Copying partition 2 content to temporary storage...."
-    print_scp_progress "Copying partition 2 content..." ./p2.raw.gz /mnt/p2.raw.gz &
-    sshpass -p 'sscl' scp ./p2.raw.gz sscl@$IP:/mnt || quit "Could not copy partition 2 content onto device. Update failed, device probably bricked."
-    cout "Copying partitions content done."
+    pretty "Copying..." "partitions content." "Mounting partition 3..." "Copying..." "mounting partition 3"
+    executeAsRoot "mount /dev/sda3 /mnt" || quit "Copying failed." "Could not mount partition 3." "Update failed." "Update failed:" "mount part 3 failed"
+    
+    pretty "Copying..." "partitions content." "Chmod partition 3..." "Copying..." "chmod partition 3"
+    executeAsRoot "chmod 777 /mnt" || quit "Copying failed." "Could not chmod partition 3." "Update failed." "Update failed:" "chmod part 3 failed"
+    
+    print_scp_progress "1" ./p1.raw.gz /mnt/p1.raw.gz &
+    sshpass -p 'sscl' scp ./p1.raw.gz sscl@$IP:/mnt || quit "Copying failed." "Could not copy p1.raw.gz." "Update failed." "Update failed:" "copy part 1 failed"
+    
+    print_scp_progress "2" ./p2.raw.gz /mnt/p2.raw.gz &
+    sshpass -p 'sscl' scp ./p2.raw.gz sscl@$IP:/mnt || quit "Copying failed." "Could not copy p2.raw.gz." "Update failed." "Update failed:" "copy part 2 failed"
+    
+    pretty "Copying..." "partitions content done." "" "Copying..." "partitions done."
 }
 
 dd_partitions() {
-    print_dd_progress "Dumping partition 1 content..." /tmp/dd1.log &
-    executeAsRoot "cat /mnt/p1.raw.gz | gzip -d - | dd of=/dev/sda1 bs=1M status=progress" > /tmp/dd1.log 2>&1  || quit "Could not dd partition 1. Update failed, device probably bricked."
+    print_dd_progress "1" /tmp/dd1.log &
+    executeAsRoot "cat /mnt/p1.raw.gz | gzip -d - | dd of=/dev/sda1 bs=1M status=progress" > /tmp/dd1.log 2>&1  || quit "Dumping failed." "Could not dump partition 1." "Update failed." "Update failed:" "dd /dev/sda1 failed"
     rm /tmp/dd1.log
     
-    print_dd_progress "Dumping partition 2 content..." /tmp/dd2.log &
-    executeAsRoot "cat /mnt/p2.raw.gz | gzip -d - | dd of=/dev/sda2 bs=1M status=progress" > /tmp/dd2.log 2>&1 || quit "Could not dd partition 2. Update failed, device probably bricked."
+    print_dd_progress "2" /tmp/dd2.log &
+    executeAsRoot "cat /mnt/p2.raw.gz | gzip -d - | dd of=/dev/sda2 bs=1M status=progress" > /tmp/dd2.log 2>&1 || quit "Dumping failed." "Could not dump partition 2." "Update failed." "Update failed:" "dd /dev/sda2 failed"
     rm /tmp/dd2.log
 
-    cout "Dumping partitions content done." 
+    pretty "Dumping..." "done." "" "Dumping done." "" 
 }
 
 unmount_tmp() {
-    cout "Unmounting temporary storage ..." 
-    executeAsRoot "umount /mnt" || quit "Could not unmount temporary storage at /mnt. Update failed, device probably bricked."
-    executeAsRoot "chmod 777 /mnt" || quit "Could not chmod /mnt. Update failed, device probably bricked."
-    cout "Unmounting temporary storage done." 
+    pretty "Unmounting..." "temporary storage ..." "" "Unmounting" "tmp"
+    executeAsRoot "umount /mnt" || quit "Unmounting failed." "Could not unmount temporary" "storage at /mnt. Update failed." "Update failed:" "umount /mnt failed"
+    executeAsRoot "chmod 777 /mnt" || quit "Changing mode failed." "Could not change permissions" " of /mnt. Update failed." "Update failed:" "chmod /mnt"
+    pretty "Unmounting..." "temporary storage" "done." "Unmounting" "tmp done."
 }
 
 copy_partition_3_content() {
-    cout "Copying partition 3 content..." "Copying partition 3 content to temporary storage...."
-    print_scp_progress "Copying partition 3 content..." ./p3.raw.gz /mnt/p3.raw.gz &
-    sshpass -p 'sscl' scp ./p3.raw.gz sscl@$IP:/mnt || quit "Could not copy partition 3 content onto device. Update failed, device probably bricked."
-    cout "Copying partition 3 content done."
+    pretty "Copying..." "...partition 3 content" "to temporary storage." "Copying" "partition 3."
+    print_scp_progress "3" ./p3.raw.gz /mnt/p3.raw.gz &
+    sshpass -p 'sscl' scp ./p3.raw.gz sscl@$IP:/mnt || quit "Copying failed." "Could not copy partition 3" "content onto device. Update failed" "Update Failed:" "copy part 3 failed"
+    pretty "Copying..." "...partition 3 content" "done." "Copying" "part 3 done."
 }
 
 dd_partition_3() {
-    print_dd_progress "Dumping partition 3 content..." /tmp/dd3.log &
-    executeAsRoot "cat /mnt/p3.raw.gz | gzip -d - | dd of=/dev/sda3 bs=1M status=progress" > /tmp/dd3.log 2>&1 || quit "Could not dd partition 3. Update failed, device probably bricked."
+    print_dd_progress "3" /tmp/dd3.log &
+    executeAsRoot "cat /mnt/p3.raw.gz | gzip -d - | dd of=/dev/sda3 bs=1M status=progress" > /tmp/dd3.log 2>&1 || quit "Dumping failed." "Could not dd partition 3." "Update failed." "Update failed:" "dump part 3 failed"
     rm /tmp/dd3.log
-    cout "Dumping partition 3 content done." 
+    pretty "Dumping partition 3..." "...done." "" "Dumping part. 3..." "...done." 
 }
 
 install_grub() {
-    cout "Finalization - unmounting ..." 
-    executeAsRoot "mount /dev/sda2 /mnt" || quit "Could not mount partition 2 for installing grub. Update failed, device probably bricked."
-    executeAsRoot "mount /dev/sda1 /mnt/boot/" || quit "Could not mount partition 1 for installing grub. Update failed, device probably bricked."
-    executeAsRoot "mount --rbind /dev /mnt/dev" || quit "Could not mount /dev for installing grub. Update failed, device probably bricked."
-    executeAsRoot "mount --rbind /sys /mnt/sys" || quit "Could not mount /sys for installing grub. Update failed, device probably bricked."
-    executeAsRoot "mount --rbind /proc /mnt/proc" || quit "Could not mount /proc for installing grub. Update failed, device probably bricked."
-    cout "Finalization - installing grub ..." 
-    executeAsRoot "chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch_grub --recheck" || quit "grub-install failed. Update failed, device probably bricked."
-    executeAsRoot "chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg" || quit "grub-mkconfig failed. Update failed, device probably bricked."
-    executeAsRoot "chroot /mnt mkinitcpio -p linux-rt" || quit "mkinitcpio failed. Update failed, device probably bricked."
-    cout "Finalization done." 
+    pretty "Finalization..." "...mounting partition" "/dev/sda2" "Finalization..." "...mounting." 
+    executeAsRoot "mount /dev/sda2 /mnt" || quit "Finalization failed" "Could not mount partition 2" "for installing grub. Update failed." "Update failed:" "mount sda2 failed"
+    pretty "Finalization..." "...mounting partition" "/dev/sda1" "Finalization..." "...mounting." 
+    executeAsRoot "mount /dev/sda1 /mnt/boot/" || quit "Finalization failed" "Could not mount partition 1" "for installing grub. Update failed." "Update failed:" "mount sda1 failed"
+    pretty "Finalization..." "...mounting partition" "/dev" "Finalization..." "...mounting." 
+    executeAsRoot "mount --rbind /dev /mnt/dev" || quit "Finalization failed" "Could not mount /dev" "for installing grub. Update failed." "Update failed:" "mount /dev failed"
+    pretty "Finalization..." "...mounting partition" "/sys" "Finalization..." "...mounting." 
+    executeAsRoot "mount --rbind /sys /mnt/sys" || quit "Finalization failed" "Could not mount /sys" "for installing grub. Update failed." "Update failed:" "mount /sys failed"
+    pretty "Finalization..." "...mounting partition" "/proc" "Finalization..." "...mounting." 
+    executeAsRoot "mount --rbind /proc /mnt/proc" || quit "Finalization failed" "Could not mount /proc" "for installing grub. Update failed." "Update failed:" "mount /proc failed"
+    pretty "Finalization..." "...installing grub." "(1/2)" "Finalization..." "...installing grub." 
+    executeAsRoot "chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch_grub --recheck" || quit "Finalization failed" "Could not install grub." "Update failed." "Update failed:" "grub-install failed"
+    pretty "Finalization..." "...installing grub." "(2/2)" "Finalization..." "...installing grub." 
+    executeAsRoot "chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg" || quit "Finalization failed" "Could not install grub." "Update failed." "Update failed:" "grub-mkconfig failed"
+    pretty "Finalization..." "...make cpio." "" "Finalization..." "...make cpio." 
+    executeAsRoot "chroot /mnt mkinitcpio -p linux-rt" || quit "Finalization failed" "Could not make cpio." "Update failed." "Update failed:" "mkinitcpio failed"
+    pretty "Finalization done." "" "" "Finalization done." "" 
 }
 
 reboot_device() {
-    cout "Rebooting ePC..."
+    pretty "Rebooting ePC..." "Please wait while" "the ePC is rebooting." "Rebooting ePC..." "Please wait."
 
     executeAsRoot "reboot"
     
@@ -195,7 +180,7 @@ reboot_device() {
         sleep 1
     done
     
-    cout "ePC has been successfully upgraded!"
+    pretty "" "Your C15 has been" "successfully upgraded!" "Your C15 has been" "successfully upgraded."
 }
 
 start_playground() {
@@ -216,7 +201,7 @@ main() {
     install_grub
     reboot_device
     start_playground
-    exit 0;
+    exit 0
 }
 
 main
